3f11894fbeeab15d269b3b7c07112ebbf5fa3129
javier581 <javierhernandezbautista710@gmail.com>
2025-05-20 07:52:15 -0600

    Add files via upload

---
+++ trabajo cl.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ void intercambio (int *a , int *b){
+    int temp;
+    temp = *b;
+    *b =*a;
+    *a = temp;
+    }
+    int main (void){
+    int x =2;
+    int y= 5;
+    printf("Antes x= %d , y =%d \n" , x , y);
+    intercambio (&x, &y);
+    printf ("después x=%d , y =%d \n",x,y);
+    return 0;
+    }
